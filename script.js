26ec45d932b18b20b9daa892296dc9576cf5da20
gabrielmatiasc-collab <gabrielmatiasc@hotmail.com>
2026-02-10 14:11:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -93,3 +93,7 @@ setInterval(criarParticula, 200);
 animar();
 
 atualizarTela();
+let visitas = localStorage.getItem("visitas") || 0;
+visitas++;
+localStorage.setItem("visitas", visitas);
+document.getElementById("visitas").innerText = visitas;
